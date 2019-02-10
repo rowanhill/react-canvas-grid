3ae5d646bad77f789455f3807a69ece5be36ccb6
Rowan Hill <rowan.hill@softwire.com>
2019-02-10 10:38:44 +0000

    Simple selection range displayed on another canvas 'layer'

--- src/index.tsx
+++ src/index.tsx
@@ -19,10 +19,24 @@ export interface ReactCanvasGridProps<T extends CellDef> {
     rowHeight: number;
 }
 
+export interface Coord {
+    x: number;
+    y: number;
+}
+
+export interface SelectRange {
+    topLeft: Coord;
+    bottomRight: Coord;
+}
+
 export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCanvasGridProps<T>, {}> {
     private readonly baseCanvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
+    private readonly highlightCanvasRef: React.RefObject<HTMLCanvasElement> = React.createRef();
     private readonly sizerRef: React.RefObject<HTMLDivElement> = React.createRef();
     private scrollParent: HTMLElement|null = null;
+
+    private selectedRangeDragStart: Coord|null = null;
+    private selectedRange: SelectRange|null = null;
 
     componentDidMount() {
         if (this.baseCanvasRef.current) {
@@ -30,7 +44,7 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
         }
         this.resizeCanvas();
         this.fixDpi();
-        this.draw();
+        this.onScroll();
 
         if (this.scrollParent && this.scrollParent !== document.body) {
             this.scrollParent.addEventListener('scroll', this.onScroll);
@@ -51,10 +65,21 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
 
     render() {
         return (
-            <div ref={this.sizerRef} >
-                <canvas ref={this.baseCanvasRef} />
+            <div ref={this.sizerRef} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} onMouseMove={this.onMouseMove}>
+                <canvas ref={this.baseCanvasRef} style={{display: 'block'}} />
+                <canvas ref={this.highlightCanvasRef} />
             </div>
         );
+    }
+
+    private calculateColumnLefts = () => {
+        const lefts = [0];
+        let curLeft = 0;
+        this.props.columns.forEach(col => {
+            curLeft += col.width;
+            lefts.push(curLeft);
+        });
+        return lefts;
     }
 
     private calculateDataSize = () => {
@@ -103,6 +128,9 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
         if (!this.baseCanvasRef.current) {
             return;
         }
+        if (!this.highlightCanvasRef.current) {
+            return;
+        }
         if (!this.sizerRef.current) {
             return;
         }
@@ -118,11 +146,14 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
 
         if (yOffset > 0 || xOffset > 0) {
             this.baseCanvasRef.current.style.transform = `translate(${xOffset}px, ${yOffset}px)`;
+            this.highlightCanvasRef.current.style.transform = `translate(${xOffset}px, ${yOffset - canvasClientRect.height}px)`;
         } else {
             this.baseCanvasRef.current.style.transform = '';
+            this.highlightCanvasRef.current.style.transform = `translate(0px, ${-canvasClientRect.height}px)`;
         }
 
-        this.draw();
+        this.drawBase();
+        this.drawHighlight();
     }
 
     private calcCanvasYOffset = (
@@ -181,17 +212,23 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
         if (!this.baseCanvasRef.current) {
             throw new Error('Cannot resize canvas: baseCanvasRef does not have current');
         }
+        if (!this.highlightCanvasRef.current) {
+            throw new Error('Cannot resize canvas: highlightCanvasRef does not have current');
+        }
         if (!this.sizerRef.current) {
             throw new Error('Cannot resize canvas: sizerRef does not have current');
         }
-        const canvas = this.baseCanvasRef.current;
+        const baseCanvas = this.baseCanvasRef.current;
+        const highlightCanvas = this.highlightCanvasRef.current;
         const sizer = this.sizerRef.current;
 
         const newCanvasSize = this.calculateMaxViewSize();
-        canvas.width = newCanvasSize.width;
-        canvas.height = newCanvasSize.height;
-        canvas.style.width = `${newCanvasSize.width}px`;
-        canvas.style.height = `${newCanvasSize.height}px`;
+        [baseCanvas, highlightCanvas].forEach(canvas => {
+            canvas.width = newCanvasSize.width;
+            canvas.height = newCanvasSize.height;
+            canvas.style.width = `${newCanvasSize.width}px`;
+            canvas.style.height = `${newCanvasSize.height}px`;
+        });
 
         const newSizerSize = this.calculateDataSize();
         sizer.style.width = `${newSizerSize.width}px`;
@@ -200,22 +237,119 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
 
     private fixDpi = () => {
         if (!this.baseCanvasRef.current) {
-            return;
+            throw new Error('Cannot resize canvas: baseCanvasRef does not have current');
         }
-        const canvas = this.baseCanvasRef.current;
+        if (!this.highlightCanvasRef.current) {
+            throw new Error('Cannot resize canvas: highlightCanvasRef does not have current');
+        }
         const dpr = window.devicePixelRatio;
 
-        const rect = canvas.getBoundingClientRect();
-        canvas.width = rect.width * dpr;
-        canvas.height = rect.height * dpr;
+        const canvases = [
+            {canvas: this.baseCanvasRef.current, alpha: false},
+            {canvas: this.highlightCanvasRef.current, alpha: true},
+        ];
 
-        // Scale all drawing operations by the dpr, so you
-        // don't have to worry about the difference.
-        var ctx = canvas.getContext('2d', { alpha: false });
-        ctx!.scale(dpr, dpr);
+        canvases.forEach(({canvas, alpha}) => {
+            const rect = canvas.getBoundingClientRect();
+            canvas.width = rect.width * dpr;
+            canvas.height = rect.height * dpr;
+    
+            // Scale all drawing operations by the dpr, so you
+            // don't have to worry about the difference.
+            var ctx = canvas.getContext('2d', { alpha });
+            ctx!.scale(dpr, dpr);
+        });
     }
 
-    private draw = () => {
+    private windowToSizer = ({x, y}: {x: number; y: number}) => {
+        if (!this.sizerRef.current) {
+            return {x, y};
+        }
+        const sizerClientRect = this.sizerRef.current.getBoundingClientRect();
+        return {
+            x: x - sizerClientRect.left,
+            y: y - sizerClientRect.top
+        };
+    }
+
+    private sizerToGrid = ({x, y}: {x: number; y: number}) => {
+        const columnLefts = this.calculateColumnLefts(); //TODO: Cache as private var
+        let colIndex = -1;
+        for (let i = 0; i < columnLefts.length; i++) {
+            if (columnLefts[i] >= x) {
+                colIndex = i - 1;
+                break;
+            }
+        }
+        return {
+            y: Math.floor(y / this.props.rowHeight),
+            x: colIndex
+        };
+    }
+
+    private gridToSizer = ({x, y}: {x: number; y: number}): ClientRect => {
+        const columnLefts = this.calculateColumnLefts(); //TODO: Cache as private var
+        return {
+            top: y * this.props.rowHeight,
+            bottom: (y + 1) * this.props.rowHeight,
+            height: this.props.rowHeight,
+            left: columnLefts[x],
+            right: columnLefts[x + 1],
+            width: this.props.columns[x].width
+        };
+    }
+
+    private onMouseDown = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const gridCoords = this.sizerToGrid(this.windowToSizer({x: event.clientX, y: event.clientY}));
+        this.selectedRangeDragStart = gridCoords;
+        this.selectedRange = {
+            topLeft: gridCoords,
+            bottomRight: gridCoords
+        };
+        this.drawHighlight();
+    }
+
+    private onMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (!this.selectedRange || !this.selectedRangeDragStart) {
+            return;
+        }
+        if ((event.buttons & 1) === 0) {
+            return;
+        }
+        const gridCoords = this.sizerToGrid(this.windowToSizer({x: event.clientX, y: event.clientY}));
+        this.selectedRange = {
+            topLeft: {
+                x: Math.min(this.selectedRangeDragStart.x, gridCoords.x),
+                y: Math.min(this.selectedRangeDragStart.y, gridCoords.y)
+            },
+            bottomRight: {
+                x: Math.max(this.selectedRangeDragStart.x, gridCoords.x),
+                y: Math.max(this.selectedRangeDragStart.y, gridCoords.y)
+            }
+        };
+        this.drawHighlight();
+    }
+
+    private onMouseUp = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        if (!this.selectedRange || !this.selectedRangeDragStart) {
+            return;
+        }
+        const gridCoords = this.sizerToGrid(this.windowToSizer({x: event.clientX, y: event.clientY}));
+        this.selectedRange = {
+            topLeft: {
+                x: Math.min(this.selectedRangeDragStart.x, gridCoords.x),
+                y: Math.min(this.selectedRangeDragStart.y, gridCoords.y)
+            },
+            bottomRight: {
+                x: Math.max(this.selectedRangeDragStart.x, gridCoords.x),
+                y: Math.max(this.selectedRangeDragStart.y, gridCoords.y)
+            }
+        };
+        this.selectedRangeDragStart = null;
+        this.drawHighlight();
+    }
+
+    private drawBase = () => {
         if (!this.baseCanvasRef.current) {
             return;
         }
@@ -226,7 +360,7 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
         }
         const sizer = this.sizerRef.current;
 
-        const baseContext = baseCanvas.getContext('2d');
+        const baseContext = baseCanvas.getContext('2d', { alpha: false });
         if (!baseContext) {
             return;
         }
@@ -299,6 +433,60 @@ export class ReactCanvasGrid<T extends CellDef> extends React.Component<ReactCan
 
         // Translate back, to bring our drawn area into the bounds of the canvas element
         baseContext.translate(translationOffset.left, translationOffset.top);
+    }
+
+    private drawHighlight = () => {
+        if (!this.highlightCanvasRef.current) {
+            return;
+        }
+        const highlightCanvas = this.highlightCanvasRef.current;
+
+        if (!this.sizerRef.current) {
+            return;
+        }
+        const sizer = this.sizerRef.current;
+
+        const highlightContext = highlightCanvas.getContext('2d', { alpha: true });
+        if (!highlightContext) {
+            return;
+        }
+
+        if (!this.scrollParent) {
+            return;
+        }
+
+        const sizerClientRect = sizer.getBoundingClientRect();
+        const scrollParentClientRect =  this.getScrollParentClientRect();
+        const canvasClientRect = highlightCanvas.getBoundingClientRect();
+
+        // Get rect to draw
+        const visibleRect = this.calculateViewRect();
+
+        // Get amount to translate
+        const translationOffset = {
+            top: this.calcCanvasYOffset(sizerClientRect, scrollParentClientRect, canvasClientRect),
+            left: this.calcCanvasXOffset(sizerClientRect, scrollParentClientRect, canvasClientRect)
+        };
+
+        // Clear the higlight layer
+        highlightContext.clearRect(0, 0, highlightCanvas.width, highlightCanvas.height);
+
+        // Translate the canvas context so that it's covering the visibleRect
+        // (so when we translate it back, what we've drawn is within the bounds of the canvas element)
+        highlightContext.translate(-translationOffset.left, -translationOffset.top);
+        
+        // Draw selected cell highlights
+        highlightContext.fillStyle = '#44aaff22';
+        highlightContext.strokeStyle = '#44aaff99';
+        if (this.selectedRange) {
+            const tl = this.gridToSizer(this.selectedRange.topLeft);
+            const br = this.gridToSizer(this.selectedRange.bottomRight);
+            highlightContext.fillRect(tl.left, tl.top, br.right - tl.left, br.bottom - tl.top);
+            highlightContext.strokeRect(tl.left, tl.top, br.right - tl.left, br.bottom - tl.top);
+        }
+
+        // Translate back, to bring our drawn area into the bounds of the canvas element
+        highlightContext.translate(translationOffset.left, translationOffset.top);
     }
 };
 
