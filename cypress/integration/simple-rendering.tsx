e330df05d46e02ebdcb22c34d43e50c745945fe8
Rowan Hill <rowan.hill@softwire.com>
2019-03-03 23:16:26 +0000

    Simple Cypress tests for scrolling and clicking

--- cypress/integration/simple-rendering.tsx
+++ cypress/integration/simple-rendering.tsx
@@ -10,16 +10,20 @@ const Holder = (props: {children?: any}) => {
   );
 };
 
-describe('ReactCanvasGrid in a overflow:scroll parent', () => {
-    it('renders a grid of data', () => {
-      const colsAndRows = createFakeDataAndColumns(100, 20, () => null);
-      const props: ReactCanvasGridProps<null> = {
-        data: colsAndRows.rows,
-        columns: colsAndRows.columns,
-        borderWidth: 1,
-        rowHeight: 20,
-      };
+const getProps = () => {
+  const colsAndRows = createFakeDataAndColumns(100, 20, () => null);
+  const props: ReactCanvasGridProps<null> = {
+    data: colsAndRows.rows,
+    columns: colsAndRows.columns,
+    borderWidth: 1,
+    rowHeight: 20,
+  };
+  return props;
+};
 
+describe('ReactCanvasGrid in an overflow:scroll parent', () => {
+    it('renders a grid of data', () => {
+      const props = getProps();
       cy.mount(<Holder><ReactCanvasGrid<null> {...props} /></Holder>, 'foo');
 
       cy.get('canvas').eq(0)
@@ -27,5 +31,34 @@ describe('ReactCanvasGrid in a overflow:scroll parent', () => {
         .should('be.greaterThan', 0);
 
       cy.get('#rcg-holder').matchImageSnapshot('simple-grid-in-scroll');
+    });
+
+    it('can be scrolled to the middle', () => {
+      const props = getProps();
+      cy.mount(<Holder><ReactCanvasGrid<null> {...props} /></Holder>, 'foo');
+
+      cy.get('canvas').eq(0)
+        .invoke('width')
+        .should('be.greaterThan', 0);
+
+      cy.get('#rcg-holder')
+        .scrollTo(300, 300);
+      cy.wait(1000); // Wait to ensure scroll bar has faded away on osx
+      cy.get('#rcg-holder')
+        .matchImageSnapshot('scrolled-grid-in-scroll');
+    });
+
+    it('renders an selection overlay over clicked cell', () => {
+      const props = getProps();
+      cy.mount(<Holder><ReactCanvasGrid<null> {...props} /></Holder>, 'foo');
+
+      cy.get('canvas').eq(0)
+        .invoke('width')
+        .should('be.greaterThan', 0);
+
+      cy.get('#rcg-holder')
+        .click();
+      cy.get('#rcg-holder')
+        .matchImageSnapshot('simple-grid-after-click');
     });
   });
