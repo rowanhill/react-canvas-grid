a7bd8ac3eacadd687665c4acdd5499a79b23c122
Rowan Hill <rowan.hill@softwire.com>
2019-09-08 17:04:37 +0100

    Remove cypress-react-unit-test - all tests are on example site now

--- cypress/components/ScrollingHolder.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export const Holder = (props: {children?: any}) => {
-    return (
-        <div id="rcg-holder" style={{width: `500px`, height: `400px`, overflow: 'scroll'}}>
-            {props.children}
-        </div>
-    );
-};
