cb020341398f31e567e41f0f0856216be41bbe96
Rowan Hill <rowan.hill@softwire.com>
2019-09-08 18:28:20 +0100

    Updates

---
+++ precache-manifest.b01c8a3b15a3eb4fc16d70ad99571104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51460cf006e3235a3020",
+    "url": "/react-canvas-grid/static/css/main.3a3a31df.chunk.css"
+  },
+  {
+    "revision": "51460cf006e3235a3020",
+    "url": "/react-canvas-grid/static/js/main.51460cf0.chunk.js"
+  },
+  {
+    "revision": "3da16eec4dcb26847c9f",
+    "url": "/react-canvas-grid/static/js/1.3da16eec.chunk.js"
+  },
+  {
+    "revision": "9f0081bcb6dd873482f2",
+    "url": "/react-canvas-grid/static/js/runtime~main.9f0081bc.js"
+  },
+  {
+    "revision": "a3b27930f33430bc352de42e853495a5",
+    "url": "/react-canvas-grid/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f069bf8e9c2a4aa9697ff7557264600a.js"
+  "/react-canvas-grid/precache-manifest.b01c8a3b15a3eb4fc16d70ad99571104.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-canvas-grid/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
