6ea7cc2b2e1f6a47fd91d75ffb9f5b502f9e9bd3
Rowan Hill <rowan.hill@gmail.com>
2020-07-12 10:39:19 +0100

    Home page tweaks

--- examples/src/Home.tsx
+++ examples/src/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as FeatureIconGradient } from './assets/feature-icon-gradient.svg';
 import { ReactComponent as RCGLogo } from './assets/logo.svg';
 import { ReactComponent as ReactLogo } from './assets/react-js.svg';
 import { ReactComponent as Rocket } from './assets/rocket-launch-lines.svg';
@@ -11,43 +12,44 @@ const Home = () => {
             <div className="hero-wrapper">
                 <RCGLogo className="logo-svg" />
                 <h1>react-canvas-grid<br/><span className="warning">(beta)</span></h1>
-                <div className="features">
-                    <div className="feature">
-                        <div className="feature-icon-sizer">
-                            <ReactLogo className="feature-icon" />
-                        </div>
-                        <h3>React-first</h3>
-                        <p>
-                            Built from the ground up for React, react-canvas-grid is designed for a simple
-                            developer experience, giving you power without getting in your way.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-sizer">
-                            <Rocket className="feature-icon" />
-                        </div>
-                        <h3>Performant</h3>
-                        <p>
-                            Powered by the canvas element, react-canvas-grid is fast - even with large data
-                            sets or complex visuals. It only draws what's visible, and only redraws what's
-                            changed, giving the user a smooth experience.
-                        </p>
-                    </div>
-                    <div className="feature">
-                        <div className="feature-icon-sizer">
-                            <Structure className="feature-icon" />
-                        </div>
-                        <h3>Rich data friendly</h3>
-                        <p>
-                            First-class support for complex data values, allowing you to easily manage
-                            compound data, metadata, or anything else!
-                        </p>
+                <div className="install-instructions-wrapper">
+                    <div className="install-instructions">
+                        <code><span className="prompt">&gt; </span>npm install --save react-canvas-grid</code>
                     </div>
                 </div>
             </div>
-            <div className="install-instructions-wrapper">
-                <div className="install-instructions">
-                    <code>npm install --save react-canvas-grid</code>
+            <div className="features">
+                <FeatureIconGradient style={{width: 0, height: 0}} />
+                <div className="feature">
+                    <div className="feature-icon-sizer">
+                        <ReactLogo className="feature-icon" />
+                    </div>
+                    <h3>React-first</h3>
+                    <p>
+                        Built from the ground up for React, react-canvas-grid is designed for a simple
+                        developer experience, giving you power without getting in your way.
+                    </p>
+                </div>
+                <div className="feature">
+                    <div className="feature-icon-sizer">
+                        <Rocket className="feature-icon" />
+                    </div>
+                    <h3>Performant</h3>
+                    <p>
+                        Powered by the canvas element, react-canvas-grid is fast - even with large data
+                        sets or complex visuals. It only draws what's visible, and only redraws what's
+                        changed, giving the user a smooth experience.
+                    </p>
+                </div>
+                <div className="feature">
+                    <div className="feature-icon-sizer">
+                        <Structure className="feature-icon" />
+                    </div>
+                    <h3>Rich data friendly</h3>
+                    <p>
+                        First-class support for complex data values, allowing you to easily manage
+                        compound data, metadata, or anything else!
+                    </p>
                 </div>
             </div>
         </>
